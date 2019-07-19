7780a7cedd9c1b80a65aa341fb34a54478f3983f
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-19 18:08:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,5 @@
 
 def countdown(n)
-
   while n > 0
      puts "#{n} SECOND(S)!"
       n -= 1
@@ -9,4 +8,10 @@ def countdown(n)
 end
 
 def countdown_with_sleep
+
+  while n > 0
+     puts "#{n} SECOND(S)!"
+      n -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
