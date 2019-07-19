c15ef9d1224168373345222b52f97b3714c24473
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-19 18:03:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 
-def countdown(number)
-  number = 0
-  while number > 0
-     number -= 1
-     puts "#{number} SECOND(S)!"
+def countdown(n)
+  n = 0
+  while n > 0
+     puts "#{n} SECOND(S)!"
+      n -= 1
   end
 end
